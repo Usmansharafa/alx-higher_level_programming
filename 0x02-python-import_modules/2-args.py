4135bacd02a39a1d0282fce12c4dbbbffe5ff27a
Usman Sharafa <Usmansharafa04@gmail.com>
2022-12-01 15:48:03 +0100

    Task 2 solved

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     arg_len = len(sys.argv)
     if arg_len == 1:
-        print("0 arguments")
+        print("0 arguments.")
     elif arg_len == 2:
         print("1 argument:")
         print(f"1: {sys.argv[1]}")
