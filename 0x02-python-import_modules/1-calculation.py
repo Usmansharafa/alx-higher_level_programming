a99c6e23228cbf0b6a70492b1fc12f9dacc76a03
Usman Sharafa <Usmansharafa04@gmail.com>
2022-12-01 15:44:21 +0100

    Task 1 solved

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as calc
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print(a, "+", b, "=", "{}".format(calc.add(a, b)))
-    print(a, "-", b, "=", "{}".format(calc.sub(a, b)))
-    print(a, "*", b, "=", "{}".format(calc.mul(a, b)))
-    print(a, "/", b, "=", "{}".format(calc.div(a, b)))
+    print(a, "+", b, "=", "{}".format(add(a, b)))
+    print(a, "-", b, "=", "{}".format(sub(a, b)))
+    print(a, "*", b, "=", "{}".format(mul(a, b)))
+    print(a, "/", b, "=", "{}".format(div(a, b)))
