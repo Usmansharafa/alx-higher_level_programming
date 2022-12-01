b4a59b523a409b9169142345ceccc858e5108f7d
Usman Sharafa <Usmansharafa04@gmail.com>
2022-12-01 15:42:16 +0100

    Task 1 solved

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ import calculator_1 as calc
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, calc.add(a, b)))
-    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calc.div(a, b)))
+    print(a, "+", b, "=", "{}".format(calc.add(a, b)))
+    print(a, "-", b, "=", "{}".format(calc.sub(a, b)))
+    print(a, "*", b, "=", "{}".format(calc.mul(a, b)))
+    print(a, "/", b, "=", "{}".format(calc.div(a, b)))
