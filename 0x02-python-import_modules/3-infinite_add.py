9437ecf4e761e4a7ca6589e095e54a5707f9f244
Usman Sharafa <usmansharafa04@gmail.com>
2022-12-03 03:00:39 -0800

    refactored code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv, exit
-    sum = 0
+    total_sum = 0
     if len(argv) == 1:
-        print(sum)
+        print(total_sum)
         exit(0)
-    arg_len = len(argv)
-    i = 1
-    while i < arg_len:
-        sum += int(argv[i])
-        i += 1
-
-    print(sum)
+    total_sum = sum([int(num) for num in argv[1:]])
+    print(total_sum)
