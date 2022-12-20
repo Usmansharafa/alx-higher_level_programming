839400013feb69b28a69b627363cd88add7f1192
Usman Sharafa <Usmansharafa04@gmail.com>
2022-12-20 21:28:04 +0100

    Task 1 solved

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""OOP Python"""
+
+
+class Square:
+    """Class representing a square"""
+    
+
+    def __init__(self, size):
+        """Initialize the class
+        
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
