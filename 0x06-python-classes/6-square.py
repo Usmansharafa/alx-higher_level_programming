87a892116d2b4740d03912bd04893401d5f2e658
Usman Sharafa <Usmansharafa04@gmail.com>
2022-12-21 00:23:34 +0100

    Task 6 solved

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,7 +17,7 @@ class Square:
 
     @property
     def size(self):
-         """int: size of square.
+        """int: size of square.
 
         Raises:
             TypeError: If size is not an integer.
@@ -35,7 +35,7 @@ class Square:
 
     @property
     def position(self):
-         """tuple: coordinate of square.
+        """tuple: coordinate of square.
 
         Raises:
             TypeError: If position is not a tuple of two positive integers.
@@ -46,7 +46,7 @@ class Square:
     def position(self, value):
         if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value 
+        self.__position = value
 
     def area(self):
         """Area module
