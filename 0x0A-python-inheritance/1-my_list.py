eaee61d178f5ea2ce7f37e5b9ced8d7d420ea378
Usman Sharafa <Usmansharafa04@gmail.com>
2023-01-10 00:53:12 +0100

    All tasks solved

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,11 @@
 
 class MyList(list):
     """Class representing MyList"""
+    
+    def __init__(self):
+        """Initializes a new list"""
+        super().__init__()
+
     def print_sorted(self):
         """Prints a list sorted in ascending order"""
         print(sorted(self))
