d537889323cec74c71e55bae75511c05434387e5
Usman Sharafa <Usmansharafa04@gmail.com>
2023-03-13 14:07:57 +0100

    Task 5 solved

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+if (!argv[2] || isNaN(parseInt(argv[2]))) { console.log('Not a number'); } else { console.log(`My number: ${parseInt(argv[2])}`); }
