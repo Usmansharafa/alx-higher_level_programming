a83272f2bbb53611fb80508400b629ef8f9a95e0
Usman Sharafa <Usmansharafa04@gmail.com>
2023-03-15 02:53:12 +0100

    Task 3 solved

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,12 +9,6 @@ module.exports = class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
-      let line = '';
-      for (let j = 0; j < this.width; j++) {
-        line += 'X';
-      }
-      console.log(line);
-    }
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 };
