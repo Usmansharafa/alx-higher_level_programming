043276b57ff5b70149777ffa8d7ca4944e53f128
Usman Sharafa <Usmansharafa04@gmail.com>
2023-03-19 00:22:21 +0100

    Task 0 solved

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 """A module that dumps the content of a database"""
 
 
-def main():
+if __name__ == "__main__":
     """This function dumps the contents of a database sorted in ascending
     order by id"""
 
@@ -12,13 +12,9 @@ def main():
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
     conn.close()
-
-
-if __name__ == "__main__":
-    main()
