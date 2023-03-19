a74170d92f6718d71fe933d7d3e8b93824dd405b
Usman Sharafa <Usmansharafa04@gmail.com>
2023-03-19 23:53:56 +0100

    Task 13 solved

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+"""Start link class to table in database
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    class State(State):
+        """A class representing a state"""
+
+        def __repr__(self):
+            """This function returns the string representation of a state"""
+            return f"{self.id}: {self.name}"
+
+    param = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
+    engine = create_engine(param, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    results = session.query(State).filter(State.name.like("%a%")).all()
+    for result in results:
+        session.delete(result)
+    session.commit()
