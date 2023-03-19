3c2a55f936b31665c2a9b95fcd8a3045ecfa91f5
Usman Sharafa <Usmansharafa04@gmail.com>
2023-03-19 18:40:53 +0100

    Task 7 solved

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 from sys import argv
 from model_state import Base, State
@@ -11,10 +11,14 @@ from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     class State(State):
+        """A class representing a state"""
+
         def __repr__(self):
+            """This function returns the string representation of a state"""
             return f"{self.id}: {self.name}"
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost\
+            /{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
