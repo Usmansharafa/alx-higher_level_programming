b713b427a7ff7a23eca4c693275c37ccf162747e
Usman Sharafa <usmansharafa04@gmail.com>
2023-07-25 23:27:35 +0000

    Task 7 solved

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const req = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+req(url, (error, response, body) => {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      req(character, (error, response, body) => {
+        if (!error) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
